1e0a84c360dfc111cdcd7756f86f884922e13689
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-01-25 19:56:37 -0300

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -13,6 +13,9 @@ function run() {
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
   core.notice("Hi from my custom JavaScript Acrion!");
+
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl); // ::set-output
 }
 
 run();
